3efc3fdd07d2484a38ee9ebb9f993e83becf1072
Nguyễn Thành Nam <namnguyenthanh.work@gmail.com>
2021-07-21 10:12:35 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,75 @@
+// Reload page
+window.addEventListener("load", () => {
+    document.querySelector("body").classList.add("loaded");
+});
+// scroll smooth
+window.addEventListener("scroll", () => {
+    const showBtn = document.querySelector(".scroll-up-btn");
+    this.scrollY > 300 ?
+        showBtn.classList.add("show") :
+        showBtn.classList.remove("show");
+})
+
+const links = document.querySelectorAll(".scroll-up-btn a");
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: true,
+        nav: false,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+});
